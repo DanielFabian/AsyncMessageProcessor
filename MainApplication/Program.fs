5af7b73d6264d8a9843583c35f5f89005633ed1f
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-08-26 03:20:43 +0200

    Added a layer with discriminated union

--- MainApplication/Program.fs
+++ MainApplication/Program.fs
@@ -42,6 +42,39 @@ let printToScreen (buffer : byte array) =
     let string = Convert.ToBase64String buffer
     printfn "%s" string 
 
+let formatData bytes =
+    let unsortedString = Convert.ToBase64String bytes
+    let unsortedChars = Array.ofSeq unsortedString
+    String (Array.sort unsortedChars)
+    
+let printFormatedToScreen (formatedMessage : string) =
+    printfn "%d characters long formated message: " formatedMessage.Length
+    printfn "%s" formatedMessage
+
+type Command =
+    | Digit of int
+    | Char of char
+    | Complex of string * int
+
+let (|Digit|_|) (str : string) = 
+    if Char.IsDigit (str.[0]) then Some(Digit(int str.[0] - int '0'))
+    else None
+let (|Char|_|) (str : string) = 
+    if str.Length >= 5 && Char.IsLetter (str.[5]) then Some(Char(str.[5]))
+    else None
+let (|Complex|_|) (str : string) = 
+    if str.StartsWith "++" then
+        let initialPlus = Seq.takeWhile (fun char -> char = '+') str |> Array.ofSeq
+        Some(Complex(String(initialPlus), str.Length))
+    else None
+
+let parseMessage message =
+    match message with
+    | Digit(digit) -> sprintf "%A" (Digit(digit))
+    | Char(char) -> sprintf "%A" (Char(char))
+    | Complex(pluses, length) -> sprintf "%A" (Complex(pluses, length))
+    | _ -> "It's baaad luck to be you..."
+
 [<EntryPoint>]
 let main args =
     printfn "Starting the trouble"
@@ -50,8 +83,12 @@ let main args =
     let pipe = FakePipe()
     let messageParts = (whenRead pipe buffer).SubscribeOn(new System.Reactive.Concurrency.EventLoopScheduler()).Select(fun numBytes -> buffer.[..numBytes-1])
     let tokenizer = Tokenizer()
-    let messages = messageParts.SelectMany(tokenizer.Tokenize)
-    messages.Subscribe(printToScreen) |> ignore
+    let messages = messageParts.SelectMany tokenizer.Tokenize
+    //messages.Subscribe printToScreen |> ignore
+    let formatedMessages = messages.Select formatData
+    //formatedMessages.Subscribe printFormatedToScreen |> ignore
+    let parsedMessages = formatedMessages.Select parseMessage
+    parsedMessages.Subscribe printFormatedToScreen |> ignore
     Console.ReadKey() |> ignore
     printfn "\nDone trouble-making"
     0
